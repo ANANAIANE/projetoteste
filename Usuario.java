7f02038d6c7506787f79531be6c588d9599acdf1
ANANAIANE <ananaiane.sf@gmail.com>
2016-01-11 16:55:31 -0200

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -1,1 +1,15 @@
-public class Usuario {}
+public class Usuario {
+
+public class Prova {
+
+    
+    public static void main(String[] args) {
+        questao_01();
+        //questao_02();
+        //questao_04();
+        //questao_05();
+    }
+    
+
+
+}
